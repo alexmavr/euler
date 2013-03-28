024f16a192e8b0d8fccf5a356772d9c74e700688
Alex Mavrogiannis <nalfemp@gmail.com>
2013-03-28 15:29:25 +0200

    solved 36

---
+++ 36/36.py
@@ -0,0 +1,20 @@
+def int_to_list(num):
+    return [int(i) for i in str(num)]
+
+def is_palindromic(num):
+    L = int_to_list(num)
+    if list(reversed(L)) == L:
+        return True
+    else:
+        return False
+
+def main():
+    s = 0
+    for i in range(1, 1000000):
+        if is_palindromic(i):
+            if is_palindromic(int(bin(i)[2:])):
+                s += i
+    print s
+
+if __name__=="__main__":
+    main()
